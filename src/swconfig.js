85d8fc2e61895a2d5fca6cff0256b8413a22aac0
Gurkirpal <gurkirpal204@gmail.com>
2019-08-06 19:38:23 +0530

    Add toast to notify app is offline ready

--- src/swconfig.js
+++ src/swconfig.js
@@ -1,9 +1,22 @@
-import { markSwWaiting, markSwNotWaiting } from "./redux/actions";
+import { toast } from 'react-toastify';
+
+import { markSwWaiting } from "./redux/actions";
+
+const onSuccess = () => {
+  toast.info('Ready to work offline', {
+    position: "bottom-center",
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: false,
+  });
+}
 
 function createConfig(dispatch) {
   return {
     onUpdate: () => dispatch(markSwWaiting()),
-    onSuccess: () => dispatch(markSwNotWaiting())
+    onSuccess: () => onSuccess(),
   }
 }
 
